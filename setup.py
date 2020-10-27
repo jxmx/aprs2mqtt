7b8f7131b796e4a941b2afc4b52a36d4db4fecbb
Jason McCormick <jason@mfamily.org>
2020-10-27 19:31:25 -0400

    build python package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hamutils", # Replace with your own username
+    version="0.0.1",
+    author="Jason McCormick",
+    author_email="jason@mfamily.org",
+    description="A collection of Ham Radio Utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jxmx/aprs-wulf",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
